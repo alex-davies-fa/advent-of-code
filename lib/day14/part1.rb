11e4528d38f7f842b35a2b8cde4d3c65e09252f8
Alex Davies <alex.davies@freeagent.com>
2022-12-14 06:58:52 +0000

    Day 14

---
+++ lib/day14/part1.rb
@@ -0,0 +1,90 @@
+require 'csv'
+require 'pp'
+require 'matrix'
+
+module Day14
+  class Part1
+    class Line
+      attr_reader :a, :b
+
+      def initialize(a, b)
+        @a = a
+        @b = b
+      end
+
+      def xs
+        [a[0],b[0]]
+      end
+
+      def ys
+        [a[1],b[1]]
+      end
+
+      def points_on_line
+        if xs[0] == xs[1]
+          (ys.min..ys.max).map { |y| Vector[xs[0], y] }
+        elsif ys[0] == ys[1]
+          (xs.min..xs.max).map { |x| Vector[x, ys[0]] }
+        end
+      end
+
+      def to_s
+        "[#{a[0]},#{a[1]}] -> [#{b[0]},#{b[1]}]"
+      end
+    end
+
+    def run(input_file)
+      lines = File.readlines(input_file)
+      rock_lines =
+        lines.flat_map do |line|
+          path =
+            line.split(" -> ").map do |coords|
+              x,y = coords.split(",").map(&:to_i)
+              Vector[x,y]
+            end
+          path.each_cons(2).map { Line.new(_1, _2) }
+        end
+
+        rocks = Set.new
+        rock_lines.each { rocks.merge(_1.points_on_line) }
+
+        max_y_pos = rocks.to_a.map { |rock| rock[1] }.max
+
+        moves = [Vector[0, 1], Vector[-1, 1], Vector[1,1], nil]
+        sand = Set.new
+        abyss = false
+
+        while !abyss do
+          blocked = false
+          sand_pos = Vector[500,0]
+          while !blocked && !abyss do
+            moves.each do |move|
+              if move.nil?
+                blocked = true
+                sand << sand_pos
+                break
+              end
+
+              new_pos = sand_pos + move
+              next if blocked?(new_pos, rocks, sand)
+
+              sand_pos = new_pos
+              break
+            end
+
+            abyss = true if sand_pos[1] >= max_y_pos
+          end
+      end
+
+      puts sand.length
+
+      nil
+    end
+
+    def blocked?(pos, rocks, sand)
+      return true if rocks.include?(pos)
+      return true if sand.include?(pos)
+      false
+    end
+  end
+end
