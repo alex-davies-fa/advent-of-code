9cdfb51d712e5987fb57b77c00d56aedd3171b41
Alex Davies <alex.davies@freeagent.com>
2022-12-21 08:24:33 +0000

    Day 20 - Part 2

--- lib/day20/part2.rb
+++ lib/day20/part2.rb
@@ -1,9 +1,67 @@
 require 'csv'
 require 'pp'
 
-module DayX
+module Day20
   class Part2
+    KEY = 811589153
+
     def run(input_file)
+      numbers = File.readlines(input_file).map(&:to_i).map { _1 * KEY }
+      indeces = (0...numbers.length).to_a
+
+      n = numbers.length
+
+      # pp n
+      # pp numbers.max
+      # pp numbers.min
+      # return
+
+      debug = false
+
+      pp numbers if debug
+      pp indeces if debug
+      puts if debug
+
+      10.times do
+        (0...n).each do |i|
+          ind = indeces.find_index(i)
+          number = numbers[ind]
+
+          puts "#{i} -> #{ind} = #{number}" if debug
+          pp numbers if debug
+
+          loops, candidate_ind = (ind + number).divmod(n)
+          new_ind = candidate_ind + loops
+
+          while !(0...n).include?(new_ind)
+            loops, candidate_ind = (new_ind).divmod(n)
+            new_ind = candidate_ind + loops
+          end
+
+          if new_ind < ind
+            numbers.delete_at(ind)
+            numbers.insert(new_ind, number)
+            old_ind = indeces.delete_at(ind)
+            indeces.insert(new_ind, old_ind)
+          else
+            old_ind = indeces.delete_at(ind)
+            indeces.insert(new_ind, old_ind)
+            numbers.delete_at(ind)
+            numbers.insert(new_ind, number)
+          end
+
+          pp numbers if debug
+          puts if debug
+        end
+
+        pp numbers if debug
+      end
+
+      z = numbers.find_index(0)
+      positions = [z+1000, z+2000, z+3000].map { _1 % n }
+      pp numbers.values_at(*positions)
+      puts numbers.values_at(*positions).sum
+      nil
     end
   end
 end
