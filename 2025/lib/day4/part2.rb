03e5b7276cf4e79938b8f423213e0173113f097b
Alex Davies <alex.davies@freeagent.com>
2025-12-04 09:26:44 +0000

    Streamline Day 4

--- 2025/lib/day4/part2.rb
+++ 2025/lib/day4/part2.rb
@@ -15,46 +15,31 @@ module Day4
     ]
 
     def run(input_file)
-      map = Hash.new(0)
-      rolls = Set.new
-
       lines = File.readlines(input_file).map { _1.strip.chars }
-      lines.each_with_index do |row,y|
+
+      rolls = Set.new
+      lines.each_with_index do |row, y|
         row.each_with_index do |c, x|
-          if c == "@"
-            rolls.add([y,x])
-            map[[y,x]] = 1
-          else
-            map[[y,x]] = 0
-          end
+          rolls.add([y,x]) if c == "@"
         end
       end
 
       init_count = rolls.length
 
-      while true
-        old_count = rolls.length
-        remove_rolls!(rolls, map)
-        break if old_count == rolls.length
+      while remove_rolls!(rolls)
       end
 
       init_count - rolls.length
     end
 
-    def remove_rolls!(rolls, map)
-      to_remove = []
-
-      rolls.each do |y,x|
-        adj = DIRS.sum do |d|
-          map[[y+d[0],x+d[1]]]
-        end
-        to_remove << [y,x] if adj < 4
+    def remove_rolls!(rolls)
+      to_remove = rolls.select do |y,x|
+        DIRS.count { |d| rolls.include?([y+d[0],x+d[1]]) } < 4
       end
 
-      to_remove.each do |p|
-        rolls.delete(p)
-        map.delete(p)
-      end
+      rolls.subtract(to_remove)
+
+      to_remove.any?
     end
   end
 end
